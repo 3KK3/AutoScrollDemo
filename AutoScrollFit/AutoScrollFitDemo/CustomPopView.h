9f1cc1e4ad51f3d039d75b91f7179be102b98e96
3kk3 <976809588@qq.com>
2017-10-16 17:34:10 +0800

    init
    init

---
+++ AutoScrollFit/AutoScrollFitDemo/CustomPopView.h
@@ -0,0 +1,13 @@
+//
+//  CustomPopView.h
+//  AutoScrollFitDemo
+//
+//  Created by YZY on 2017/10/16.
+//  Copyright © 2017年 YZY. All rights reserved.
+//
+
+#import "PopupBaseView.h"
+
+@interface CustomPopView : PopupBaseView
+
+@end
---
+++ AutoScrollFit/AutoScrollFitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoScrollFitDemo
+//
+//  Created by YZY on 2017/10/16.
+//  Copyright © 2017年 YZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
